93f5c493b28cffef96d90b39f5f49e6fdb7e1484
LuanaCamposA <35103187+LuanaCamposA@users.noreply.github.com>
2018-11-01 09:49:54 -0300

    Add files via upload

---
+++ tcpClient.py
@@ -0,0 +1,28 @@
+from socket import*
+
+serverName = 'servername'
+serverPort = 12000
+##cria o socket do cliente; o primeiro parametro indica que a rede subjacente está
+#usando ipv4 e o sock_stream indica que é uma conexão tcp
+clientSocket = socket(AF_INET, SOCK_STREAM)
+##essa linha estabele a conexão com o socket servidor
+#por meio do connect é executada a apresentação de 3 vias
+#e uma conexão tcp é estabelecida
+clientSocket.connect((serverName,serverPort))
+
+#essa linha obtem uma sentença do usuario, ate que o user digite um enter
+sentence = raw_input('Input lowercase sentence')
+
+#essa linha envia a cadeia sentence pelo socket do cliente e para a conexão tcp
+#
+clientSocket.send(sentence)
+
+#quando os caracteres chegam do servidor, eles são colocados na cadeia Modified
+#Sentence até que a linha termine
+modifiedSentence = clientSocket.recv(1024)
+#printa a sentenca recebida em maiuscula do servidor, fechamos o socket do cliente
+print 'from Server', modifiedSentence
+##encerra a conexao tcp e fecha o socket
+##ela faz o tcp do cliente enviar uma mensagem tcp ao tcp no servidor
+clientSocket.close()
+
