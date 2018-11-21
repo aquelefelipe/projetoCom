dadb9f91c0984905b9551d9941fc9ab9132be419
Luana Campos <luana.campos.a@gmail.com>
2018-11-21 10:19:07 -0300

    Alterando dnsServer e dnsClient

--- tcpServer.py
+++ tcpServer.py
@@ -1,10 +1,10 @@
-
 import socket, json
 
 serverPort = 12000
-serverHost = '172.22.67.194'
-
+serverHost = '192.168.0.13' #LUANA
+#serverHost = '192.168.0.21'  #FELIPE
 nomes = b''
+response = b''
 
 datas = [ 
                 {
@@ -24,7 +24,7 @@ datas = [
 
                 {
                         "nome": "Dudu",
-                        "descricao": "Leticia so pega no pé do coitado"
+                        "descricao": "Leticia so pega no pe do coitado"
                 },
 
                 {
@@ -35,7 +35,7 @@ datas = [
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket:
     serverSocket.bind((serverHost, serverPort))         ##associamos o num de porta do servidor, serverPort, ao socket
-    serverSocket.listen(1)                              ##esse parametro especifica o num de conexoes tcp na fila; e o listen()
+    serverSocket.listen(3)                              ##esse parametro especifica o num de conexoes tcp na fila; e o listen()
                                                         ##faz com que o servidor escute as requisições tcp do cliente
 
     print ('This server is ready to receive')
@@ -43,38 +43,52 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket:
                                                         ##connectioSocket vai ser um novo socket dedicado a esse cliente especifico
         connectionSocket, addr = serverSocket.accept()
                                                         #o servidor vai enviar sentence como resposta
-        print('connected by', addr)
-        sentence = connectionSocket.recv(1024)
-        if not sentence: break        
-        #sentence.decode()
+        print(f'connected by', addr)
+
+        while True:
+                sentence = connectionSocket.recv(1024)
+                if not sentence: break        
+                print(sentence.decode()[:7])
+                print(sentence.decode()[8:])
+                #DECODIFICAÇÃO DA SOLICITAÇÃO
+                if sentence.decode() == "LISTAR" :
+                        nomes = b''
+                        for x in datas:
+                                nomes += bytes(x["nome"], "utf-8")
+                                nomes += b' '
+                        connectionSocket.send(nomes)
+
+                elif sentence.decode()[:7] == "ARQUIVO" :
+                        response = b''
+                        for x in datas:
+                                if sentence.decode()[8:] == x["nome"] :
+                                        response = bytes(x["descricao"], "utf-8")
+                                
+                        connectionSocket.send(response)
+
+                elif sentence.decode() == "ENCERRAR":
+                        connectionSocket.sendall(b'conexao encerrada')
+                        print(f'encerrando conexão com cliente {addr}')
+                        break
+                else:
+                        response = b''
+                        response = b'Operacao nao identificada pelo Servidor'
+                        connectionSocket.send(response)
         
-        #DECODIFICAÇÃO DA SOLICITAÇÃO
-        if sentence.decode() == "LISTAR" :
-                for x in datas:
-                        nomes += bytes(x["nome"], "utf-8")
-                        nomes += b' '
-                connectionSocket.sendall(nomes)
-
-        elif sentence.decode()[0:6] == "ARQUIVO" :
-                for x in datas:
-                        if sentence.decode()[8:] == x["nome"] :
-                                connectionSocket.sendall(bytes(x["descricao"], "utf-8"))
-
-        elif sentence.decode() == "ENCERRAR":
-                connectionSocket.sendall(b'conexao encerrada')
-                connectionSocket.close()
-                print("encerrando conexão com cliente ${addr}", addr)
-                break
-
-
+        connectionSocket.close()
+        break
         
 
 ##nesse programa, apos ser enviada a sentença modificada, fechamos o socket da conexao
 #mas o serverSocket permanece aberto, dai outro cliente pode "bater na porta"
 #e enviar uma sentence ao servidor p que seja modificada
 
-
 #FUNÇÕES DO SERVIDOR
 # 1. Listar arquivos do servidor CÓDIGO 1
 # 2. Sinalizar que arquivo não existe 
 # 3. Enviar arquivo para cliente solicitante CÓDIGO 2
+
+#012345678910
+#ENCERRAR
+#LISTAR
+#ARQUIVO X
