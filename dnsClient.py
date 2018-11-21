d4c4047ae987412c079b4d172f3bc79f3f40d18c
Luana Campos <luana.campos.a@gmail.com>
2018-11-21 10:05:08 -0300

    Alterando dnsServer e dnsCliente

---
+++ dnsClient.py
@@ -0,0 +1,64 @@
+
+import socket
+
+nome = '172.22.67.194'
+porta = 12000
+#message = "hello, world!"
+print('Digite site desejado:')
+message = input(" ")
+
+sok =  socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+sok.sendto(bytes(message, 'utf-8'), (nome,porta)) 
+while True:    
+    print('Waiting to receive...')
+    dado, addr = sok.recvfrom(2048)
+    print (f'Endereço de luana: \n', dado.decode())
+    #print(f'Recebido do DNS {addr}')
+    print('Closing socket...')
+    break
+
+'''
+serverName = '172.22.67.194'
+serverPort = 12000
+##cria o socket do cliente; o primeiro parametro indica que a rede subjacente está
+#usando ipv4 e o sock_stream indica que é uma conexão tcp
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientSocket:
+##essa linha estabele a conexão com o socket servidor
+#por meio do connect é executada a apresentação de 3 vias
+#e uma conexão tcp é estabelecida
+    clientSocket.connect((serverName,serverPort))
+#essa linha obtem uma sentença do usuario, ate que o user digite um enter
+#essa linha envia a cadeia sentence pelo socket do cliente e para a conexão tcp
+
+    #while True:
+    request = input(" ")
+    print(request)
+    clientSocket.sendall(bytes(request, 'utf-8'))
+    print(request)
+    modifiedSentence = clientSocket.recv(1024) #quando os caracteres chegam do servidor, eles são colocados na cadeia ModifiedSentence até que a linha termine
+    #print(f'from Server: ', modifiedSentence.decode())  #printa a sentenca recebida em maiuscula do servidor
+    #str1 = 'ENCERRAR'
+    print(request)
+    if request == "ENCERRAR":
+    #if modifiedSentence.decode() == request:
+        print('Encerrando conexão\n')
+        clientSocket.close()  ##encerra a conexao tcp e fecha o socket ela faz o tcp do cliente enviar uma mensagem tcp ao tcp no servidor
+
+    elif request == "LISTAR":
+    #elif modifiedSentence.decode() == 'LISTAR' :
+        #print (f'Lista de arquivos do servidor: ', modifiedSentence.decode())
+        #i = 0
+        #while i<5:
+        print('recebendo arquivos:\n') 
+        #arquivo = clientSocket.recv(1024)
+        print (f'Arquivo: \n', modifiedSentence.decode())
+            #i += 1
+        #print('Digite proximo comando :')
+    
+    elif request[0:6] == "ARQUIVO": ##Digite qualquer outra coisa
+        #print('Qual o arquivo que você deseja?')
+        #clientSocket.sendall(bytes(input(" "), 'utf-8'))
+        
+        #novoDados = clientSocket.recv(1024)
+        print (f'Dados: ', modifiedSentence.decode())
+'''
--- dnsServer.py
+++ dnsServer.py
@@ -1,180 +1,29 @@
-import socket, glob, json
 
-port = 80 ##pq o dns opera na porta 53
-ip = '172.22.67.194' ## é o ip do proprio pc
+import socket, json
 
+serverHost = '172.22.67.194' #LUANA IP
+serverPort = 12000
+end = "teste123"
+
+##TO USANDO O dnsclient.py na pasta documentos pra testar
+    
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
-sock.bind((ip, port))
+sock.bind((serverHost, serverPort))         ##associamos o num de porta do servidor, serverPort, ao socket                            
 
-def load_zone():
-
-    jsonzone = {}
-    zonefiles = glob.glob('zones/*.zone')
-    
-    for zone in zonefiles:
-        with open(zone) as zonedata:
-            data = json.load(zonedata)
-            zonename = data["$origin"]
-            jsonzone[zonename] = data
-
-    return jsonzone
-zonedata = load_zone()
-
-def getflags(flags):
-
-    byte1 = bytes(flags[0:1])
-    byte2 = bytes(flags[1:2])
-
-    rflags = ''
-
-    QR = '1'
-    OPCODE = ''
-    for bit in range(1,5):
-        OPCODE += str(ord(byte1)&(1<<bit)) ##pega todos os bits do opcode
-
-    AA = '1'
-
-    TC = '0'
-
-    RD = '0'
-
-    RA = '0'
-
-    Z = '000'
-
-    RCODE = '0000' #RESPONSE CODE
-
-    return int(QR+OPCODE+AA+TC+RD, 2).to_bytes(1,byteorder = 'big')+int(RA+Z+RCODE, 2).to_bytes(1, byteorder = 'big') 
-
-def getquestiondomain(data):
-    state = 0
-    expectedlenght = 0
-    domainstring = ''
-    domainparts = []
-    x = 0
-    y = 0
-
-    for byte in data:
-        if state == 1:
-            if byte != 0:
-                domainstring += chr(byte)
-            x += 1
-            if x == expectedlenght:
-                domainparts.append(domainstring)
-                domainstring = ''
-                state = 0
-                x = 0
-            if byte == 0:
-                domainparts.append(domainstring)
-                break
-        else:
-            state = 1
-            expectedlenght = byte
-  
-        y += 1 
-
-    questiontype = data[y:y+2]
-
-    # print(domainparts)
-
-    return (domainparts, questiontype) #vai retonar as duas partes do dominio
-
-def getzone(domain):
-    global zonedata
-
-    zone_name = '.'.join(domain)
-    return zonedata[zone_name]
-
-def getrecs(data):
-    domain, questiontype = getquestiondomain(data)
-    qt = ''
-    if questiontype == b'\x00\x01':
-        qt = 'a'
-
-    zone = getzone(domain)
-
-    return (zone[qt], qt, domain)
-
-def buildquestion(domainname, rectype):
-    qbytes = b''
-
-    for part in domainname:
+while True: 
+        #print('Entrei no while')
+        data, addr = sock.recvfrom(2048) 
+        print(f'Recebendo dados from: {addr}')
+        dt = data.decode()
+        print(f'cliente mandou: ', dt) 
+        break
+while True:
+        print('Entrei no SEGUNDO while')
+        sock =  socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        if dt == 'luana': #to testando com o site = luana, ai só digita luana no cliente
+                print('dt == luana')
+                message = "lua123" #seria o endereço do site solicitado
+                sock.sendto(bytes(message, 'utf-8'), (serverHost,serverPort))
+        break
         
-        length = len(part)
-        qbytes += bytes([length])
-        
-        for char in part:
-            qbytes += ord(char).to_bytes(1, byteorder = 'big')
-
-    if rectype == 'a':
-        qbytes += (1).to_bytes(2, byteorder = 'big')
-
-    qbytes += (1).to_bytes(1, byteorder = 'big')
-
-    return qbytes
-
-def rectobytes(domainname, rectype, recttl, recval):
-
-    rbytes = b'\xc0\x0c' ##representa a compression order de howcode.org
-
-    if rectype == 'a':
-        rbytes = rbytes + bytes([0]) + bytes([1])
-
-    rbytes = rbytes + bytes([0]) + bytes([1])
-
-    rbytes += int(recttl).to_bytes(4, byteorder = 'big')
-
-    if rectype == 'a':
-        rbytes = rbytes + bytes([0]) + bytes([4])
-
-        #pra printar a ultima parte: o ipv4 endereço
-        for part in recval.split('.'):
-            rbytes += bytes([int(part)])
-
-    return rbytes
-
-def buildresponse(data):
-    
-    #Transaction ID
-    TransactionID = data[0:2] 
-
-    #Get the Flags
-    Flags = getflags(data[2:4])
-
-    print(Flags)
-
-    #Question Count (num de perguntas q o servidor vai responder... default = 1 )
-    QDCOUNT = b'\x00\x01'
-
-    #Aswer Count (vai ser de acordo com quantas respostas o servidor dns vai dar, nesse caso
-    # vão ser 3 respostas)
-  
-    ANCOUNT = len(getrecs(data[12:])[0]).to_bytes(2, byteorder = 'big')
-    
-    #NameServer Count
-    NSCOUNT = (0).to_bytes(2, byteorder = 'big')
-
-    #Additional Count
-    ARCOUNT = (0).to_bytes(2, byteorder = 'big')
-
-    dnsheader = TransactionID+Flags+QDCOUNT+ANCOUNT+NSCOUNT+ARCOUNT
-    #print(dnsheader) #dá o endereço em hexa
-
-    #construindo DNS body
-    dnsbody = b'' 
-
-    #Resposta pra requisição
-    records, rectype, domainname = getrecs(data[12:])
-
-    dnsquestion = buildquestion(domainname, rectype)
-
-    for record in records:
-        dnsbody += rectobytes(domainname, rectype, record["ttl"], record["value"])
-
-    return dnsheader + dnsquestion + dnsbody
-
-
-while 1:
-    data, addr = sock.recvfrom(512) #
-    r = buildresponse(data)
-    sock.sendto(r, addr) # vai enviar a resposta r pro addr, nosso end 
+#TA FALTANDO CONFERIR CONFIABILIDADE
